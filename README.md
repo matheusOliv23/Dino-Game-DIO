c2537f0f6a966d4bd591ed66039d3b39668a42c8
matheusOliv23 <matheushenri1996@gmail.com>
2021-09-16 23:41:36 -0300

    projeto dio finalizado

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dio Dino Game
+
+Dino game for Digital Innovation One.
+
+![screenshot](example.png?raw=true 'screenshot')
---
+++ main.js
@@ -0,0 +1,67 @@
+const dino = document.querySelector('.dino')
+const background = document.querySelector('.background')
+
+let isGameOver = false
+let position = 0
+let isJumping = false
+
+function handleKeyup(event) {
+  if (event.keyCode === 32) {
+    if (!isJumping) {
+      jump()
+    }
+  }
+}
+
+function jump() {
+  isJumping = true
+
+  let upInterval = setInterval(() => {
+    if (position >= 150) {
+      clearInterval(upInterval)
+
+      let downInterval = setInterval(() => {
+        if (position <= 0) {
+          clearInterval(downInterval)
+          isJumping = false
+        } else {
+          position -= 20
+          dino.style.bottom = position + 'px'
+        }
+      }, 20)
+    } else {
+      position += 20
+      dino.style.bottom = position + 'px'
+    }
+  }, 20)
+}
+
+function createCactos() {
+  const cactus = document.createElement('div')
+  let cactusPosition = 1000
+  let randomTime = Math.random() * 6000
+
+  if (isGameOver) return
+
+  cactus.classList.add('cactus')
+  cactus.style.left = 1000 + 'px'
+  background.appendChild(cactus)
+
+  let leftInterval = setInterval(() => {
+    if (cactusPosition < -60) {
+      clearInterval(leftInterval)
+      background.removeChild(cactus)
+    } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+      clearInterval(leftInterval)
+      document.body.innerHTML = '<h1 class="game-over">Fim do Jogo</h1>'
+    } else {
+      cactusPosition -= 10
+      cactus.style.left = cactusPosition + 'px'
+    }
+  }, 20)
+
+  setTimeout(createCactos, randomTime)
+}
+
+createCactos()
+document.addEventListener('keyup', handleKeyup)
